43984454830322e36e8aca1fa27aee3748fafe05
Seale Rapolai <rapolaiseale@gmail.com>
2018-09-05 09:46:13 +0200

    Complete Album Reducer

--- musicman-front/src/_reducers/album.reducer.js
+++ musicman-front/src/_reducers/album.reducer.js
@@ -2,6 +2,7 @@ import { albumConstants } from '../_constants';
 
 export function albums(state = [], action) {
   switch (action.type) {
+    //Create
     case albumConstants.ALBUM_CREATE_REQUEST:
       return {
         ...state,
@@ -21,7 +22,95 @@ export function albums(state = [], action) {
         createSuccess: false,
         loading: false,
         error: action.error
-      }
+      };
+
+    //GetAll
+    case albumConstants.ALBUM_GETALL_REQUEST:
+      return {
+        ..state,
+        loading: true
+      };
+    case albumConstants.ALBUM_GETALL_SUCCESS:
+      return {
+        ..state,
+        loading: false,
+        getAllSuccess: true,
+        error: action.error,
+        albums: action.albums
+      };
+    case albumConstants.ALBUM_GETALL_FAILURE:
+      return {
+        ..state,
+        loading: false,
+        getAllSuccess: false,
+        error: action.error
+      };
+
+    //GetById
+    case albumConstants.ALBUM_GETBYID_REQUEST:
+      return {
+        ..state,
+        loading: true
+      };
+    case albumConstants.ALBUM_GETBYID_SUCCESS:
+      return {
+        ..state,
+        loading: false,
+        getByIdSuccess: true,
+        error: action.error,
+        album: action.album
+      };
+    case albumConstants.ALBUM_GETBYID_FAILURE:
+      return {
+        ..state,
+        loading: false,
+        getByIdSuccess: false,
+        error: action.error
+      };
+
+    //Update
+    case albumConstants.ALBUM_UPDATE_REQUEST:
+      return {
+        ..state,
+        loading: true
+      };
+    case albumConstants.ALBUM_UPDATE_SUCCESS:
+      return {
+        ..state,
+        loading: false,
+        updateSuccess: true,
+        error: action.error,
+        album: action.album
+      };
+    case albumConstants.ALBUM_UPDATE_FAILURE:
+      return {
+        ..state,
+        loading: false,
+        updateSuccess: false,
+        error: action.error
+      };
+
+    //Delete
+    case albumConstants.ALBUM_DELETE_REQUEST:
+      return {
+        ..state,
+        loading: true
+      };
+    case albumConstants.ALBUM_DELETE_SUCCESS:
+      return {
+        ..state,
+        loading: false,
+        deleteSuccess: true,
+        error: action.error,
+        status: action.stat
+      };
+    case albumConstants.ALBUM_DELETE_FAILURE:
+      return {
+        ..state,
+        loading: false,
+        deleteSuccess: false,
+        error: action.error
+      };
 
     default:
       return state;
